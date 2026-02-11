8603c71d9a0819793c7b5575d13640d8ceacf252
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-02-11 11:09:30 +0100

    feat(AddBookView): Ajout de la page AddBookView.vue

--- p-web-db-294-frontend/src/router/index.ts
+++ p-web-db-294-frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import WorksView from '../views/WorksView.vue'
 import AboutView from '../views/AboutView.vue'
+import AddBookView from '../views/AddBookView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/works',
       name: 'works',
       component: WorksView,
+    },
+    {
+      path: '/add-book',
+      name: 'add-book',
+      component: AddBookView,
     },
   ],
 })
---
+++ p-web-db-294-frontend/src/views/AddBookView.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="book_add">
+    <h1>Ajout d'un livre</h1>
+    <form action="POST" class="add_book_form">
+      <label for="titre">Titre</label>
+      <input type="text" id="titre">
+
+
+      <label for="categorie">Categorie</label>
+      <input list="categorie">
+      <datalist id="categorie">
+        <option value="test"></option>
+        <option value="test2"></option>
+      </datalist> 
+
+      
+      <label for="num_pages">Nombre de pages</label>
+      <input type="number" id="num_pages">
+
+      
+      <label for="extrait_livre">Extrait du livre</label>
+      <input type="file" id="extrait_livre">
+      
+
+      <label for="resume">Résumé</label>
+      <input type="text" id="resume">
+
+
+      <label for="nom_auteur">Nom de l'auteur</label>
+      <input type="text" id="nom_auteur">
+
+
+      <label for="prenom_auteur">Prénom de l'auteur</label>
+      <input type="text" id="prenom_auteur">
+
+
+      <label for="annee_edition">Année d'édition</label>
+      <input type="number" id="annee_edition">
+
+
+      <label for="moyenne_apprecition">Moyenne d'appéciations</label>
+      <input type="number" id="moyenne_apprecition">
+
+
+      <label for="commentaires">Commentaires</label>
+      <input type="text" id="commentaires">
+
+      
+      <label for="image_couverture">Image de couverture</label>
+      <input type="file" id="image_couverture">
+    </form>
+  </div>
+</template>
+
+<style scoped>
+  .add_book_form {
+    display: grid;
+    grid-template-columns: 1fr, 1fr;
+    
+  }
+</style>
