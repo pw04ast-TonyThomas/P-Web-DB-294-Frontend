fd69e3c43c588d4fbd2e109738fdd9e9b3d16041
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2026-02-11 10:03:40 +0100

    Feat(route): change le bouton pour amener au ouvrages
    [2]

--- p-web-db-294-frontend/src/App.vue
+++ p-web-db-294-frontend/src/App.vue
@@ -1,65 +1,62 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import SearchBar from './components/SearchBar.vue';
+import SearchBar from './components/SearchBar.vue'
 </script>
 
 <template>
   <div class="wrapper">
-
-    <header style="grid-area: header;">
-        <nav>
-          <RouterLink to="/">L&CloseCurlyQuote;0ctet Littéraire</RouterLink>
-          <RouterLink to="/about">Ouvrages</RouterLink>
-        </nav>
-        <SearchBar></SearchBar>
+    <header style="grid-area: header">
+      <nav>
+        <RouterLink to="/">L&CloseCurlyQuote;0ctet Littéraire</RouterLink>
+        <RouterLink to="/works">Ouvrages</RouterLink>
+      </nav>
+      <SearchBar></SearchBar>
     </header>
-    <main style="grid-area: main;">
+    <main style="grid-area: main">
       <RouterView />
     </main>
-    <footer style="grid-area: footer;">
-
-    </footer>
+    <footer style="grid-area: footer"></footer>
   </div>
 </template>
 
 <style scoped>
-  .wrapper {
-    min-height: 100%;
+.wrapper {
+  min-height: 100%;
 
-    display: grid;
-    grid-template:
+  display: grid;
+  grid-template:
     'header header  header' 4rem
     'top-svg top-svg top-svg'
     'main    main    main' auto
     'bot-svg bot-svg bot-svg'
     'footer  footer  footer'
     / 2fr 8fr 2fr;
-    place-items: start center;
-  }
+  place-items: start center;
+}
 
-  header {
-    place-self: center;
-    width: 100%;
-    height: 100%;
+header {
+  place-self: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  background: var(--secondary);
+  border: 4px solid var(--main);
+  box-sizing: border-box;
+
+  & > nav {
     display: flex;
-    justify-content: space-evenly;
-    align-items: center;
+    gap: 1rem;
+    & a {
+      color: var(--third);
+      text-decoration: none;
+    }
 
-    background: var(--secondary);
-    border: 4px solid var(--main);
-    box-sizing: border-box;
-
-    & > nav {
-      display: flex;
-      gap: 1rem;
-      & a {
-        color: var(--third);
-        text-decoration: none;
-      }
-
-      & a:hover {
-        text-decoration: underline;
-      }
+    & a:hover {
+      text-decoration: underline;
     }
   }
+}
 </style>
