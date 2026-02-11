cf99ed6ed273f099d77a162ce6333deb2a07e190
bwbl <theophile2006@gmail.com>
2026-02-11 09:13:32 +0100

    feat: Started the header and remove some of the default file [30]

--- p-web-db-294-frontend/src/App.vue
+++ p-web-db-294-frontend/src/App.vue
@@ -1,85 +1,61 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="wrapper">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <header style="grid-area: header;">
+        <nav>
+          <RouterLink to="/">L&CloseCurlyQuote;0ctet Littéraire</RouterLink>
+          <RouterLink to="/about">Ouvrages</RouterLink>
+        </nav>
+        <SearchBar></SearchBar>
+    </header>
+    <main style="grid-area: main;">
+      <RouterView />
+    </main>
+    <footer style="grid-area: footer;">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    </footer>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+  .wrapper {
+    height: 100%;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+    display: grid;
+    grid-template:
+    'header header  header' 4rem
+    'top-svg top-svg top-svg'
+    'main    main    main' auto
+    'bot-svg bot-svg bot-svg'
+    'footer  footer  footer'
+    / 2fr 8fr 2fr;
+    place-items: start center;
+  }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
   header {
+    place-self: center;
+    width: 100%;
+    height: 100%;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    justify-content: space-evenly;
+    align-items: center;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    background: var(--secondary);
+    border: 4px solid var(--main);
+    box-sizing: border-box;
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    & > nav {
+      display: flex;
+      gap: 1rem;
+      & a {
+        color: var(--third);
+        text-decoration: none;
+      }
+    }
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ p-web-db-294-frontend/src/components/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="search" name="search" id="search" class="search" placeholder="search">
+</template>
+
+<style scoped>
+  input {
+    width: 20rem;
+    padding: 0.5rem;
+
+    border: 1px solid var(--secondary-2);
+    border-radius: 5px;
+    box-sizing: content-box;
+
+    background: var(--main);
+
+    transition: all 200ms;
+  }
+
+  input:focus {
+    border: 2px solid var(--third);
+    outline: none;
+  }
+</style>
