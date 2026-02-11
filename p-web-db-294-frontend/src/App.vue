e5c3f9c3aef3bac3959cda9aebdeeb84ffd41e0a
bwbl <theophile2006@gmail.com>
2026-02-11 11:24:21 +0100

    chore: prettier [5]

--- p-web-db-294-frontend/src/App.vue
+++ p-web-db-294-frontend/src/App.vue
@@ -8,17 +8,17 @@ import SearchBar from './components/SearchBar.vue'
     <header style="grid-area: header">
       <nav>
         <RouterLink to="/">L&CloseCurlyQuote;0ctet Littéraire</RouterLink>
-          <span></span>
+        <span></span>
         <RouterLink to="/works">Ouvrages</RouterLink>
         <RouterLink to="/add-book">addbook</RouterLink>
       </nav>
       <SearchBar></SearchBar>
     </header>
-    <img class="svg top" style="grid-area: top-svg" src="./assets/waves-top.svg" alt="">
+    <img class="svg top" style="grid-area: top-svg" src="./assets/waves-top.svg" alt="" />
     <main style="grid-area: main">
       <RouterView />
     </main>
-    <img class="svg bot" style="grid-area: bot-svg" src="./assets/waves-bot.svg" alt="">
+    <img class="svg bot" style="grid-area: bot-svg" src="./assets/waves-bot.svg" alt="" />
     <footer style="grid-area: footer">
       <p>&copy; L&CloseCurlyQuote;0ctet Littéraire</p>
     </footer>
@@ -40,53 +40,53 @@ import SearchBar from './components/SearchBar.vue'
   place-items: start center;
 }
 
-  header {
-    place-self: center;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
+header {
+  place-self: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 
   background: var(--secondary);
   border: 4px solid var(--main);
   box-sizing: border-box;
 
+  gap: 1rem;
+  & > nav {
+    display: flex;
     gap: 1rem;
-    & > nav {
-      display: flex;
-      gap: 1rem;
-      & a {
-        color: var(--third);
-        text-decoration: none;
-      }
+    & a {
+      color: var(--third);
+      text-decoration: none;
+    }
 
-      & a:hover {
-        text-decoration: underline;
-      }
+    & a:hover {
+      text-decoration: underline;
+    }
 
-      & span::after {
-        content: '○';
-      }
+    & span::after {
+      content: '○';
     }
   }
+}
 
-  .svg {
-    width: 100%;
-    height: 50vh;
-  }
+.svg {
+  width: 100%;
+  height: 50vh;
+}
 
-  .top {
-    transform: scaleY(-1);
-    transform-origin: center;
-  }
+.top {
+  transform: scaleY(-1);
+  transform-origin: center;
+}
 
-  footer {
-    background: #000000;
-    height: 100%;
-    width: 100%;
-    color: #ffffff;
-    padding: 1rem;
-    box-sizing: border-box;
-  }
+footer {
+  background: #000000;
+  height: 100%;
+  width: 100%;
+  color: #ffffff;
+  padding: 1rem;
+  box-sizing: border-box;
+}
 </style>
--- p-web-db-294-frontend/src/components/SearchBar.vue
+++ p-web-db-294-frontend/src/components/SearchBar.vue
@@ -1,24 +1,24 @@
 <template>
-  <input type="search" name="search" id="search" class="search" placeholder="search">
+  <input type="search" name="search" id="search" class="search" placeholder="search" />
 </template>
 
 <style scoped>
-  input {
-    width: 20rem;
-    padding: 0.5rem;
+input {
+  width: 20rem;
+  padding: 0.5rem;
 
-    border: 1px solid var(--secondary-2);
-    border-radius: 5px;
-    box-sizing: content-box;
+  border: 1px solid var(--secondary-2);
+  border-radius: 5px;
+  box-sizing: content-box;
 
-    background: url('../assets/icons/search.svg') center right 0.5rem no-repeat;
-    background-color: var(--main);
+  background: url('../assets/icons/search.svg') center right 0.5rem no-repeat;
+  background-color: var(--main);
 
-    transition: all 200ms;
-  }
+  transition: all 200ms;
+}
 
-  input:focus {
-    border: 2px solid var(--third);
-    outline: none;
-  }
+input:focus {
+  border: 2px solid var(--third);
+  outline: none;
+}
 </style>
