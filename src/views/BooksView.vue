195042f4b3298b483861303b3dc4e95c6d1225fe
bwbl <theophile2006@gmail.com>
2026-02-24 16:28:15 +0100

    feat: started the books view

--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -3,6 +3,17 @@
 <template>
   <main>
     <h1>Ouvrages</h1>
+    <li>
+      <ul>
+        <a href=""></a>
+      </ul>
+      <ul>
+        <a href=""></a>
+      </ul>
+      <ul>
+        <a href=""></a>
+      </ul>
+    </li>
   </main>
 </template>
 
