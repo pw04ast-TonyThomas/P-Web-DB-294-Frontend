118a42ff7b4b84566010e67025332450ef2b9478
bwbl <theophile2006@gmail.com>
2026-02-24 17:20:28 +0100

    feat: Add an exemple card for the booksview

---
+++ src/components/CardItem.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+defineProps(['src', 'title', 'rating'])
+const getStars = (rating: number) => {
+  const max = 5
+  const fullStars = Math.round(Math.min(Math.max(rating, 0), max))
+  const emptyStars = max - fullStars
+
+  return '★'.repeat(fullStars) + '☆'.repeat(emptyStars)
+}
+</script>
+
+<template>
+  <div class="card">
+    <img :src="src" alt="" />
+    <h3>{{ title }}</h3>
+    <h4>{{ getStars(rating) }}</h4>
+  </div>
+</template>
+
+<style scoped>
+.card > img {
+  height: 10rem;
+  aspect-ratio: 1 / 1;
+}
+</style>
--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -1,20 +1,75 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CardItem from '@/components/CardItem.vue'
+</script>
 
 <template>
   <main>
     <h1>Ouvrages</h1>
-    <li>
-      <ul>
-        <a href=""></a>
-      </ul>
-      <ul>
-        <a href=""></a>
-      </ul>
-      <ul>
-        <a href=""></a>
-      </ul>
-    </li>
+    <ul class="nav">
+      <li>
+        <a href="">Ajouter un ouvrage</a>
+      </li>
+      <li>
+        <a href="">Modifier un ouvrage</a>
+      </li>
+      <li>
+        <a href="">Supprimer un ouvrage</a>
+      </li>
+    </ul>
+    <div class="category">
+      <legend>Fantaisie</legend>
+      <CardItem src="https://cataas.com/cat" title="Harry Potter le 3" rating="4"></CardItem>
+    </div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.category {
+  width: 70vw;
+}
+.nav {
+  display: flex;
+  gap: 1rem;
+  li,
+  a {
+    color: var(--third);
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+}
+
+legend {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-size: 1.5rem;
+  font-family: 'JetBrainsMono', sans-serif;
+  color: #333;
+  gap: 1rem;
+  margin-bottom: 1rem;
+}
+
+legend::before,
+legend::after {
+  content: '';
+  flex: 1;
+  height: 0.5rem;
+  background-color: var(--secondary);
+  border-radius: 10px;
+}
+
+legend::before {
+  flex: 0 0 50px;
+}
+.nav > li:not(:first-child)::before {
+  content: '○';
+  margin-right: 1rem;
+  font-weight: bold;
+}
+
+.nav > li::marker {
+  content: none;
+}
+</style>
