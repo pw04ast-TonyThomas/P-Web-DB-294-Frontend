433e2cd534be9f0d7f71df3a9f4e295bd503dba1
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-03-04 09:53:47 +0100

    feat(BookModify): Ajout de la page de modification de book et ajout de sa route dans le router
    [20][DONE]

---
+++ src/views/BookModify.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// @ts-nocheck
+import { onMounted, ref } from 'vue'
+import Service from '@/services/service.js'
+import starIcon from '../assets/icons/star.svg'
+import starEmptyIcon from '../assets/icons/starEmpty.svg'
+</script>
+
+<template>
+  <main class="main">
+    <a>test</a>
+  </main>
+</template>
+
+<style scoped></style>
